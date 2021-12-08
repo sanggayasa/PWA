8a690216281b02ea10e2a3560ae915760c70bcc4
sanggayasa <sanggayasa502@gmail.com>
2021-12-08 10:31:46 +0700

    menyiapkan API dan data

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'cb9a539c42141325182a82f990b8540e',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
