232b65e7171aa81ebc68803c0780fefd2d1f930c
sanggayasa <sanggayasa502@gmail.com>
2021-12-08 09:09:39 +0700

    membuat dan configurasi drawer app shell

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /*
 masukan modul dengan format seperti ini
 import modul_name from 'module_lookasi';
@@ -6,5 +7,10 @@ import modul_name from 'module_lookasi';
 import 'regenerator-runtime';
 import '../styles/style.css';
 import '../styles/responsive.css';
+import App from './views/app';
 
-console.log('Hello Coders!');
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+});
