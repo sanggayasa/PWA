d50ff38d3ea953a732997c4af4a6200cf415e0bb
sanggayasa <sanggayasa502@gmail.com>
2021-12-08 08:58:47 +0700

    membuat apps shell

---
+++ src/scripts/app.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-underscore-dangle */
+class App {
+  constructor({ button, drawer, content }) {
+    this._button = button;
+    this._drawer = drawer;
+    this._content = content;
+  }
+}
+
+export default App;
