d8f4dad60ea5a619854893ff9587a6ce8dff0457
sanggayasa <sanggayasa502@gmail.com>
2021-12-08 10:35:22 +0700

    Setelah menyiapkan data source kita bisa langsung panggil fungsi di dalam TheMovieDbSource sesuai dengan halamannya masing-masing pada afterRender().

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,3 +1,6 @@
+import UrlParser from '../../routes/url-parser';
+import TheMovieDbSource from '../../data/themoviedb-source';
+
 const Detail = {
   async render() {
     return `
@@ -7,6 +10,9 @@ const Detail = {
 
   async afterRender() {
     // Fungsi ini akan dipanggil setelah render()
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const movie = await TheMovieDbSource.detailMovie(url.id);
+    console.log(movie);
   },
 };
 
