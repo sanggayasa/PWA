865783cf452164d9ff43538119dd22c1a5f800be
sanggayasa <sanggayasa502@gmail.com>
2021-12-09 11:13:44 +0700

    membuat websocket yang berfungsi untuk sinkronisasi client dengan server secara ascync agar data dapat di update secara berkala setiap 10 detik sekali pada halaman now playing , bisa di cek pada console

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,6 +9,8 @@ import '../styles/style.css';
 import '../styles/responsive.css';
 import App from './views/app';
 import swRegister from './utils/sw-register';
+import WebSocketInitiator from './utils/websocket-initiator';
+import CONFIG from './globals/config';
 
 const app = new App({
   button: document.querySelector('#hamburgerButton'),
@@ -23,4 +25,5 @@ window.addEventListener('hashchange', () => {
 window.addEventListener('load', () => {
   app.renderPage();
   swRegister();
+  WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
 });
---
+++ src/scripts/utils/websocket-initiator.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-underscore-dangle */
+const WebSocketInitiator = {
+  init(url) {
+    const webSocket = new WebSocket(url);
+    webSocket.onmessage = this._onMessageHandler;
+  },
+
+  _onMessageHandler(message) {
+    console.log(message.data);
+  },
+};
+export default WebSocketInitiator;
